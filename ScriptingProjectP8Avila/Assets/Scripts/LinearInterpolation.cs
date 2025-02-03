66f520b855f902dce18a41849990197d77c16e86
Astroworld2023 <723948@student.dusd.net>
2025-02-03 15:25:34 -0800

    Started the 3D gamekit

---
+++ ScriptingProjectP8Avila/Assets/Scripts/LinearInterpolation.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinearInterpolation : MonoBehaviour
+{
+    /*
+
+    // In this case, the result would equal 4
+    float result = Mathf.Lerp (3f, 5f, 0.5f);
+
+    Vector3 from = new Vector3 (1f, 2f, 3f,);
+    Vector3 to = new Vector3 (5f, 6f, 7f);
+
+    // Here result = (4, 5, 6)
+    Vector3 to = Vector3.Lerp (from, to, 0.75f);
+    
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    // This line of code makes it so that a light gets more intense the more frames that passes.
+    The rate of the intensity slows as the targets gets closer
+    light.intensity = Mathf.Lerp(light.intensity, 8f, 0.5f);
+       S// This line of code makes it so that a light gets more intense each second that passes
+        light.intensity = Mathf.Lerp(light.intensity, 8f, 0.5f * Time.deltaTime);
+
+    
+    }
+
+    */
+}
